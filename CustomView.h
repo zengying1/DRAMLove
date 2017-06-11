383270574998ca9b6bb23c91fa0ef8f6d227d409
Oreal51 <Oreal51@Oreal51deMacBook-Pro.local>
2017-06-12 00:21:27 +0800

    first commit

---
+++ CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  DrawLoveImage
+//
+//  Created by Oreal51 on 2017/6/11.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  DrawLoveImage
+//
+//  Created by Oreal51 on 2017/6/11.
+//  Copyright © 2017年 Oreal51. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
